2d4caae05a1cc5faaa3b853385d944c5841cf51d
tingxins <tingxins@sina.com>
2017-06-05 17:20:41 +0800

    [ADD] The header file of InputKit.

---
+++ InputKit/InputKit.h
@@ -0,0 +1,15 @@
+//
+//  InputKit.h
+//  InputKitDemo
+//
+//  Created by tingxins on 05/06/2017.
+//  Copyright © 2017 tingxins. All rights reserved.
+//
+
+#ifndef InputKit_h
+#define InputKit_h
+#import "TXLimitedTextField.h"
+#import "TXLimitedTextView.h"
+
+
+#endif /* InputKit_h */
