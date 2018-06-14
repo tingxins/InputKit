bb6c2b68831255f2a8eb597773dd29c26e248d16
sergey petrachkov <petrachkovsergey@gmail.com>
2018-06-14 08:41:30 +0700

    move float separator to a separate enum that can be configured at runtime and update match manager price method accordingly

--- InputKitSwift/MatchManager.swift
+++ InputKitSwift/MatchManager.swift
@@ -10,18 +10,18 @@ import Foundation
 
 internal class MatchManager {
     
-    class func matchLimitedTextTypePrice(component:AnyObject, value: String) -> Bool {
-        
-        guard let limitedPrefix = component.value(forKeyPath: "limitedPrefix") as? Int, let limitedSuffix = component.value(forKeyPath: "limitedSuffix") as? Int else {
-            return true
-        }
-        
-        let matchZero = NSPredicate(format: MatchHeader.Name.kRegExHeader, MatchConstant.Name.kLimitedTextZeroRegEx)
-        let matchValue = NSPredicate(format: MatchHeader.Name.kRegExHeader, "^\\d{0,\(limitedPrefix)}$|^(\\d{0,\(limitedPrefix)}[.][0-9]{0,\(limitedSuffix)})$")
-        let isZero = !matchZero.evaluate(with: value)
-        let isCorrectValue = matchValue.evaluate(with: value)
-        return isZero && isCorrectValue ? true : false
+  class func matchLimitedTextTypePrice(component:AnyObject, value: String) -> Bool {
+    
+    guard let limitedPrefix = component.value(forKeyPath: "limitedPrefix") as? Int, let limitedSuffix = component.value(forKeyPath: "limitedSuffix") as? Int else {
+      return true
     }
+    
+    let matchZero = NSPredicate(format: MatchHeader.Name.kRegExHeader, MatchConstant.Name.kLimitedTextZeroRegEx)
+    let matchValue = NSPredicate(format: MatchHeader.Name.kRegExHeader, "^\\d{0,\(limitedPrefix)}$|^(\\d{0,\(limitedPrefix)}[\(MatchVariables.kLimitedTextNumberFloatSeparators)][0-9]{0,\(limitedSuffix)})$")
+    let isZero = !matchZero.evaluate(with: value)
+    let isCorrectValue = matchValue.evaluate(with: value)
+    return isZero && isCorrectValue ? true : false
+  }
     
     class func matchLimitedTextTypeCustom(regEx: String, component: AnyObject, value: String) -> Bool {
         let matchValue = NSPredicate(format: MatchHeader.Name.kRegExHeader, regEx)
